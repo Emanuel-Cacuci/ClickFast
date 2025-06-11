7340c69720ef9a193b4e5ea4c5aba9beb691b9b5
emanuelcacucipro@gmail.comgit config --global user.email emanuelcacucipro@gmail.com <emanuelcacucipro@gmail.com>
2025-06-11 10:59:07 +0200

    suite

--- script.js
+++ script.js
@@ -23,43 +23,25 @@ function updateDisplay(val) {
 
 // TIMMER
 
-var h1 = document.getElementsByTagName('h1')[0];
-var start = document.getElementById('strt');
-var stop = document.getElementById('stp');
-var reset = document.getElementById('rst');
-var sec = 0;
-var min = 0;
-var hrs = 0;
-var t;
+var myVar = setInterval(function(){ myTimer() }, 1000);
+var secondlimit = 10;
 
-function tick() {
-  sec++;
-  if (sec >= 60) {
-    sec = 0;
-    min++;
-    if (min >= 60) {
-      min = 0;
-      hrs++;
-    }
-  }
-}
-function add() {
-  tick();
-  h1.textContent = (hrs > 9 ? hrs : '0' + hrs) + ':' +
-      (min > 9 ? min : '0' + min) + ':' + (sec > 9 ? sec : '0' + sec);
-  timer();
-}
-function timer() {
-  t = setTimeout(add, 1000);
+function myTimer() {
+if(secondlimit == 0)
+{
+    myStopFunction();
 }
 
-timer();
-start.onclick = timer;
-stop.onclick = function() {
-  clearTimeout(t);
-} reset.onclick = function() {
-  h1.textContent = '00:00:00';
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
+document.getElementById("safeTimerDisplay").innerHTML = '00:' + zeroPad(secondlimit,2);
+secondlimit = secondlimit  - 1;
+
+}
+
+function myStopFunction() {
+    clearInterval(myVar);
+}
+
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
 }
