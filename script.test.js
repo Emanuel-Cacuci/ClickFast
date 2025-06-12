e0ea93faf9226f0497532e055633a6c5d339b85d
emanuelcacucipro@gmail.comgit config --global user.email emanuelcacucipro@gmail.com <emanuelcacucipro@gmail.com>
2025-06-12 15:20:16 +0200

    script.test.js

---
+++ script.test.js
@@ -0,0 +1,12 @@
+const {
+  // Mes éléments à importer
+} = require("chemin_vers_mon_script.js");
+
+
+
+document.body.innerHTML = `
+  <div id="score">0</div>
+  <div id="timer">5</div>
+  <button id="button-clicker">Click me!</button>
+  <button id="button-reset">Reset</button>
+`;
