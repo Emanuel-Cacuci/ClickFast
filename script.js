bc5e26da07c05eea563c92d27981f8f71608bebd
emanuelcacucipro@gmail.comgit config --global user.email emanuelcacucipro@gmail.com <emanuelcacucipro@gmail.com>
2025-06-12 16:49:11 +0200

    ok

--- script.js
+++ script.js
@@ -6,39 +6,10 @@
 
 // CLICK
 
-let counterVal = 0;
+  let counterVal = 0;
 
-function incrementClick() {
-  updateDisplay(++counterVal);
-}
-
-function resetCounter() {
-  counterVal = 0;
-  updateDisplay(counterVal);
-}
-
-function updateDisplay(val) {
-  document.getElementById('counter-label').innerHTML = val;
-}
-
-// TIMMER
-
-  var myVar = setInterval(function(){ myTimer() }, 1000);
-  var secondlimit = 10;
-
-  function myTimer() {
-  if(secondlimit == 0)
-  {
-      myStopFunction();
-  }
-
-  document.getElementById("safeTimerDisplay").innerHTML = '00:' + zeroPad(secondlimit,2);
-  secondlimit = secondlimit  - 1;
-
-  }
-
-  function myStopFunction() {
-      clearInterval(myVar);
+  function incrementClick() {
+    updateDisplay(++counterVal);
   }
 
   function resetCounter() {
@@ -50,8 +21,37 @@ function updateDisplay(val) {
     document.getElementById('counter-label').innerHTML = val;
   }
 
+  // TIMMER
 
-  function zeroPad(num, places) {
-    var zero = places - num.toString().length + 1;
-    return Array(+(zero > 0 && zero)).join("0") + num;
-  }
+    var myVar = setInterval(function(){ myTimer() }, 1000);
+    var secondlimit = 10;
+
+    function myTimer() {
+    if(secondlimit == 0)
+    {
+        myStopFunction();
+    }
+
+    document.getElementById("safeTimerDisplay").innerHTML = '00:' + zeroPad(secondlimit,2);
+    secondlimit = secondlimit  - 1;
+
+    }
+
+    function myStopFunction() {
+        clearInterval(myVar);
+    }
+
+    function resetCounter() {
+      counterVal = 0;
+      updateDisplay(counterVal);
+    }
+
+    function updateDisplay(val) {
+      document.getElementById('counter-label').innerHTML = val;
+    }
+
+
+    function zeroPad(num, places) {
+      var zero = places - num.toString().length + 1;
+      return Array(+(zero > 0 && zero)).join("0") + num;
+    }
